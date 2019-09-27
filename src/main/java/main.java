7117f50a53a7e4ef916e26b7d58b399204f122a3
michele-bortolini <michele.bortolini3@gmail.com>
2019-09-27 15:53:13 -0300

    Test

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args){
         HelloWorld helloWorld = new HelloWorld();
-        System.out.println(helloWorld.Hello());
+        System.out.println(helloWorld.Hello()+"Michele");
     }
 
 
